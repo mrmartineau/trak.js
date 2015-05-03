3e2bcfe69b543b20f27113752be5b2692b2320f5
Zander Martineau <zander@martineau.tv>
2015-05-03 13:13:28 +0100

    add jasmine tests. incomplete implementation at the moment

---
+++ spec/trak/trakSpec.js
@@ -0,0 +1,56 @@
+describe("trak", function() {
+	var trak = require('../../src/trak.js');
+
+	beforeEach(function() {
+		global.document = {
+			title: 'Page title',
+			referrer: 'http://google.com'
+		};
+		global.window = {
+			location : {
+				href: 'http://google.com'
+			}
+		}
+	});
+
+	describe('Wildcards', function () {
+		it("returns the page title", function() {
+			expect(trak.wildcard('page.title')).toEqual('Page title');
+		});
+
+		it("returns the page href", function() {
+			expect(trak.wildcard('page.href')).toEqual('http://google.com');
+		});
+
+		it("returns the referrer", function() {
+			expect(trak.wildcard('referrer')).toEqual('http://google.com');
+		});
+	});
+
+	xdescribe('trak.event', function () {
+		var gaMock = {
+			category: 'CATEGORY',
+			action: 'ACTION',
+			label: 'LABEL' ,
+			value: '1',
+			eventName: 'click'
+		};
+
+		beforeEach(function () {
+			global.ga = function(type, event, category, action, label, value) {
+			};
+		});
+
+		it('should emit and construct an Object that matches gaMock and call ga() with it', function () {
+			trak.event({
+				category: 'CATEGORY',
+				action: 'ACTION',
+				label: 'LABEL',
+				value: 1
+			});
+
+			expect(ga).toHaveBeenCalledWith('send', 'event', 'CATEGORY', 'ACTION', 'LABEL');
+		});
+
+	});
+});
