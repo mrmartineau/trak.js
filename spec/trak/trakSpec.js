bb5e8a3e585c9adf44a3b7fcbdd22ad0f7318158
Zander Martineau <zander@martineau.tv>
2015-05-04 09:05:13 +0100

    `trak.event()` tests now pass but need more work

--- spec/trak/trakSpec.js
+++ spec/trak/trakSpec.js
@@ -1,7 +1,7 @@
 describe("trak", function() {
 	var trak = require('../../dist/trak.js');
 
-	beforeEach(function() {
+	beforeAll(function() {
 		global.document = {
 			title: 'Page title',
 			referrer: 'http://google.com'
@@ -41,29 +41,52 @@ describe("trak", function() {
 	});
 
 
-	xdescribe('trak.event', function () {
+	describe('trak.event', function () {
 		var gaMock = {
 			category: 'CATEGORY',
 			action: 'ACTION',
 			label: 'LABEL' ,
-			value: '1',
+			value: 1,
 			eventName: 'click'
 		};
+		global._gaq = [];
+		global.dataLayer = [];
 
-		beforeEach(function () {
-			global.ga = function(type, event, category, action, label, value) {
-			};
+		beforeAll(function () {
+			ga = jasmine.createSpy('ga');
 		});
 
+		afterEach(function() {
+			trak.options.trackType = 'ga';
+		});
+
+
 		it('should emit and construct an Object that matches gaMock and call ga() with it', function () {
-			trak.event({
-				category: 'CATEGORY',
-				action: 'ACTION',
-				label: 'LABEL',
-				value: 1
+			trak.event(gaMock);
+
+			expect(ga).toHaveBeenCalledWith('send', 'event', 'category', 'action', 'label', 1, { nonInteraction: false });
 			});
 
-			expect(ga).toHaveBeenCalledWith('send', 'event', 'CATEGORY', 'ACTION', 'LABEL');
+
+		it('should emit & push to the _gaq array', function () {
+			trak.options.trackType = '_gaq';
+			trak.event(gaMock);
+
+			expect(_gaq).toContain(['_trackEvent', 'category', 'action', 'label', 1]);
+		});
+
+
+		it('should emit and & push to the dataLayer array', function () {
+			trak.options.trackType = 'gtm';
+			trak.event(gaMock);
+
+			expect(dataLayer).toContain({
+				eventCategory: 'category',
+				eventAction: 'action',
+				eventLabel: 'label' ,
+				eventValue: 1,
+				event: 'click'
+			});
 		});
 
 	});
